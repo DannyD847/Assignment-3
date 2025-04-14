24c174268f2ba7b53421f448b58a65ce63053854
DannyD847 <dannydv847@gmail.com>
2025-04-14 19:34:38 -0400

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,12 @@
+from math import *
+
+number = int(input('Enter a number: '))
+
+square_root = sqrt(number)
+Logarithm = log(number)
+sine_value = sin(number)
+
+print('Square root: ',square_root)
+print('Logarithm: ',Logarithm)
+print('Sine: ',sine_value)
+
